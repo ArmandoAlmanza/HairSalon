a9881b0cf6bd1a84545a77b950a2a529a32648d3
Armando Almanza <armando.s.almanza@gmail.com>
2021-05-17 17:06:48 -0500

    add some js code

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+	App();
+});
+
+const App = () => {
+	showServices();
+};
+
+async function showServices(){
+	try {
+		const result = await fetch('./servicios.json');
+		const db = await result.json();
+		const { servicios } = db;
+	
+		servicios.forEach( service => {
+			const { id, name, price } = service;
+		});
+
+	} catch (error) {
+		console.log(error);
+	}
+}
--- build/js/index.js
+++ build/js/index.js
@@ -1,2 +1,2 @@
-
+document.addEventListener("DOMContentLoaded",()=>{App()});const App=()=>{showServices()};async function showServices(){try{const o=await fetch("./servicios.json"),c=await o.json(),{servicios:e}=c;e.forEach(o=>{const{id:c,name:e,price:s}=o})}catch(o){console.log(o)}}
 //# sourceMappingURL=index.js.map

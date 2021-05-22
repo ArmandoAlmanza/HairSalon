41bd0f09ed276f09958181287a63003fdf32d382
Armando Almanza <armando.s.almanza@gmail.com>
2021-05-22 17:01:11 -0500

    add the services

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
-	App();
+    App();
 });
 
 const App = () => {
-	showServices();
+    showServices();
 };
 
-async function showServices(){
-	try {
-		const result = await fetch('./servicios.json');
-		const db = await result.json();
-		const { servicios } = db;
-	
-		servicios.forEach( service => {
-			const { id, name, price } = service;
+async function showServices() {
+    try {
+        const result = await fetch("./servicios.json");
+        const db = await result.json();
+        const { servicios } = db;
 
-			const servceName;
-		});
+        servicios.forEach((service) => {
+            const { id, name, price } = service;
 
-	} catch (error) {
-		console.log(error);
-	}
+            const servceName = document.createElement("p");
+            servceName.textContent = name;
+            servceName.classList.add("service-name");
+
+            const servicePrice = document.createElement("p");
+            servicePrice.textContent = `$ ${price}`;
+            servicePrice.classList.add("service-price");
+
+            const serviceDiv = document.createElement("div");
+            serviceDiv.classList.add("service");
+            serviceDiv.dataset.serviceID = id;
+
+            serviceDiv.onclick = selectService;
+
+            serviceDiv.appendChild(servceName);
+            serviceDiv.appendChild(servicePrice);
+
+            document.querySelector("#servicio").appendChild(serviceDiv);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function selectService(e) {
+    let element;
+    if (e.target.tagName === "P") {
+        element = e.target.parentElement;
+    } else {
+        element = e.target;
+    }
+
+    if (element.classList.contains("select")) {
+        element.classList.remove("select");
+    } else {
+        element.classList.add("select");
+    }
 }
